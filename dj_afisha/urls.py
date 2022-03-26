3504f3ae635b4b18eeabd35c4fb825cf33034ebf
kasssya <kasietkubanychekova1@gmail.com>
2022-03-26 17:20:34 +0600

    hw2

--- dj_afisha/urls.py
+++ dj_afisha/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('api/v1/movies/<int:id>/', views.movie_item_view),
     path('api/v1/reviews/', views.reviews_list_view),
     path('api/v1/reviews/<int:id>/', views.review_item_view),
+    path('api/v1/movies/reviews/', views.movies_reviews_view),
 ]
